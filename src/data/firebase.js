99256e0be0f89c4f725f679441c287c49465603c
rhr18818 <rhr14101@gmail.com>
2026-04-04 21:59:45 +0600

    Secured Firebase API keys by pulling from .env file

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0NMQ77E_2KogoZKtpwlDYf7QvJ6doJYA",
-  authDomain: "bdt-collect.firebaseapp.com",
-  projectId: "bdt-collect",
-  storageBucket: "bdt-collect.firebasestorage.app",
-  messagingSenderId: "585117205650",
-  appId: "1:585117205650:web:7f724d6f7ad3211793124b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
